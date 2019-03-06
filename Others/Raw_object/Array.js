5c39d5cd00197a35facdfacc78287d1a2d27c119
teal.yao <teal.yao@corp.to8to.com>
2019-03-06 16:33:30 +0800

    add array reduce snippets

--- Others/Raw_object/Array.js
+++ Others/Raw_object/Array.js
@@ -1,8 +1,61 @@
-// flatten array
+/// flatten array
+// 1. absolute flatten, no depth param
 // [[1,[2,[[3]]]],4,[5,[[[6]]]]] => [1,2,3,4,5,6]
 const flatten = list => list.reduce(
-    (a, b) => a.concat(Array.isArray(b) ? flatten(b) : b), []
+	(a, b) => a.concat(Array.isArray(b) ? flatten(b) : b), []
 );
+// 2. with depth param
+function flatten(arr, depth = 1) {
+	return arr.reduce((newArr, nextArr) => newArr.concat(depth > 1 && Array.isArray(nextArr) ? flatten(nextArr, depth - 1) : nextArr), [])
+}
+
+/// compose
+/**
+ * github: https://github.com/reduxjs/redux/blob/master/src/compose.js
+ * @params {...function} [funcs] 函数
+ */
+function compose(...funcs) {
+	if (funcs.length === 0) {
+		return arg => arg
+	}
+	if (funcs.length === 1) {
+		return funcs[0]
+	}
+	return funcs.reduce((a, b) => (...args) => a(b(...args)))
+}
+// 分解
+let fn = compose(
+	returnC,
+	returnB,
+	returnA
+)
+fn('D')
+// core 
+[returnC, returnB, returnA].reduce((fn, nextFn) => {
+	return (...args) => fn(nextFn(...args))
+})
+// 最后嵌套的函数
+fn = (...args) => returnC(returnB(returnA(...args)))
+const returnA = letter => {
+	console.log(letter)
+	return 'A'
+}
+const returnB = letter => {
+	console.log(letter)
+	return 'B'
+}
+const returnC = letter => {
+	console.log(letter)
+	return 'C'
+}
+let lastLetter = compose(
+	returnC,
+	returnB,
+	returnA
+)('D')
+console.log(lastLetter) // C
+// output: D A B C
+
 
 /// reduce mock
 // recusive， 不支持reducer的第三四个参数即index, array
@@ -18,33 +71,33 @@ function reduce(arr, acc, callback) {
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
 Object.defineProperty(Array.prototype, 'reduce', {
 	value: function (callback) {
-		  var o = Object(this);
-		  var len = o.length >>> 0; 
-		  var k = 0; 
-		  var value;
-	
-		  if (arguments.length >= 2) {
+		var o = Object(this);
+		var len = o.length >>> 0;
+		var k = 0;
+		var value;
+
+		if (arguments.length >= 2) {
 			value = arguments[1];
-		  } else {
+		} else {
 			while (k < len && !(k in o)) {
-			  k++; 
+				k++;
 			}
-	
+
 			if (k >= len) {
-			  throw new TypeError( 'Reduce of empty array ' +
-				'with no initial value' );
+				throw new TypeError('Reduce of empty array ' +
+					'with no initial value');
 			}
 			value = o[k++];
-		  }
-	
-		  while (k < len) {
+		}
+
+		while (k < len) {
 			if (k in o) {
-			  value = callback(value, o[k], k, o);
-			}	
+				value = callback(value, o[k], k, o);
+			}
 			k++;
-		  }
-	
-		  return value;
+		}
+
+		return value;
 	}
 })
 
@@ -53,25 +106,30 @@ var is_array = function (value) {
 	/*return value&&
 		typeof value === "object"&&
 		value.constructor === Array;*/ //得value所在的window对象跟当前的window是同一个。不同的页面框架(frames)有不同的Window对象
-	return Object.prototype.toString.call(value) === "[object Array]";	
+	return Object.prototype.toString.call(value) === "[object Array]";
 };
 //数组的初始化构造
 new Array(num);
 
 Array.dim = function (dimention, initial) {
-		var a = [], i;
-		for (i = 0; i<dimention; i+=1) {
-			a[i] = initial;	
-		}
-		return a;
+	var a = [],
+		i;
+	for (i = 0; i < dimention; i += 1) {
+		a[i] = initial;
+	}
+	return a;
 };
 var a5 = Array.dim(5, 0);
 
 
 //Array.prototype.reduce && Array.prototype.reduceRight
-var dupArray = [[1, 3], [2, 4], [5, 7]];
+var dupArray = [
+	[1, 3],
+	[2, 4],
+	[5, 7]
+];
 var ret2 = dupArray.reduce(function (memory, value) {
-  return memory.concat(value);
+	return memory.concat(value);
 }, [9]);
-console.log("ret2: " , ret2);
+console.log("ret2: ", ret2);
 //=> [9, 1, 3, 2, 4, 5, 7]
