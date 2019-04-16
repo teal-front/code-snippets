2ea8f7f17f64ba67c5ad5e2b717b6e5e20a79872
teal-front <yaowang1@qq.com>
2019-04-16 23:30:39 +0800

    fix: quicksort

--- algorithm/sort/quicksort.js
+++ algorithm/sort/quicksort.js
@@ -3,21 +3,23 @@
  * 默认对数字排序，怎么选择初始的基准数可以衍生很多版本
  */
 exports.quickSort =  function quickSort(arr) {
-    if (arr.length <= 1) return arr;
+    if (arr.length <= 1) return arr
 
     let index = 0, // 选择了数组第1个为基准值
-        middle = arr.splice(index, 1);
-    let left = [], right = [];
-
-    for (let item of arr) {
-        if (item > middle[0]) {
+        cur = arr[index],
+        middle = [],
+        left = [],
+        right = []
+    let i = -1, l = arr.length, item
+    while(++i < l) {
+        item = arr[i]
+        if (item > cur) {
             right.push(item)
-        } else if (item < middle[0]) {
+        } else if (item < cur) {
             left.push(item)
         } else {
             middle.push(item)
         }
     }
-
-    return quickSort(left).concat(middle, quickSort(right));
+    return quickSort(left).concat(middle, quickSort(right))
 }
