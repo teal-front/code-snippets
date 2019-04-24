352a70ed46faaa4b9f0251fa27429b62c2479b80
teal.yao <teal.yao@corp.to8to.com>
2019-04-24 09:27:14 +0800

    add pali partition

--- Others/Raw_object/String.js
+++ Others/Raw_object/String.js
@@ -19,25 +19,58 @@ function toUnicode(theString) {
  * https://leetcode.com/problems/palindrome-partitioning/submissions/ 
  * 
  */
-var partition = function(string) {
-  let ret  = []
+var partition = function (string) {
+  let ret = []
   dfs(string, [])
   return ret
 
-  function dfs (string, array) {
-      if (string === '') {
-          ret.push(array.slice())
-          return
+  function dfs(string, array) {
+    if (string === '') {
+      ret.push(array.slice())
+      return
+    }
+    // '23'.slice(0, '23'.length) => '23'
+    // 故下面 i <= l
+    for (let i = 1, l = string.length; i <= l; i++) {
+      if (isPali(string.slice(0, i))) {
+        array.push(string.slice(0, i))
+        dfs(string.slice(i), array)
+        array.pop()
       }
-      for(let i = 1, l = string.length + 1; i < l; i++) {
-          if (isPali(string.slice(0, i))) {
-              array.push(string.slice(0, i))
-              dfs(string.slice(i), array)
-              array.pop()
-          }
-      }
+    }
   }
+
   function isPali(string) {
     return string === string.split('').reverse().join('')
   }
 };
+console.log(partition('2332'))
+
+/**
+ * 字符串转数字
+ * 
+ * 要注意正负数的判断，不考虑超过最大整数情况
+ * 
+ * 34 => 
+ * (((0 * 10 + 3) * 10) + 4)
+ */
+function string2int(string) {
+  if (string === null || string.length === 0) {
+    return 0
+  }
+  let isNegative = string.charAt(0) === '-'
+  let i = -1,
+    l = string.length,
+    ret = 0,
+    c
+  // also would use `reduce`
+  while (++i < l) {
+    if (i === 0 && isNegative) {
+      continue
+    }
+    c = Number(string.charAt(i))
+
+    ret = ret * 10 + c
+  }
+  return isNegative ? -ret : ret
+}
